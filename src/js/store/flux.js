4209f52fb5aac1a9078b6331c1ccc4e9fa698735
leisyarcia <leisyarcia@gmail.com>
2020-07-07 22:21:14 +0000

    working on signup

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -19,13 +19,16 @@ const getState = ({ getStore, getActions, setStore }) => {
             actions: {
 			signup: async (email, password) => {
 				const response = await fetch(
-					"https://3000-c0f8905b-5e5c-4fc3-a510-7f2618865c6e.ws-us02.gitpod.io/registration",
+					"https://3000-c0f8905b-5e5c-4fc3-a510-7f2618865c6e.ws-us02.gitpod.io/signup",
 					{
 						method: "POST",
 						headers: {
 							"Content-Type": "application/json"
 						},
 						body: JSON.stringify({
+                            business_name: business_name,
+                            address: address,
+                            phone_number: phone_number,
 							email: email,
 							password: password
 						})
--- src/js/views/signup.js
+++ src/js/views/signup.js
@@ -4,12 +4,20 @@ import { Redirect } from "react-router-dom";
 export const SignupView = function() {
 	const { actions, store } = useContext(Context);
 	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+    const [password, setPassword] = useState("");
+    const [business_name, setBusiness_name] = useState("");
+    const [phone_number, setPhone_number] = useState("");
+    const [address, setAddress] = useState("");
+
+
 	return (
 		<div>
-			{store.token ? <Redirect to="/registration" /> : ""}
-			<input type="text" placeholder="enter your email" onChange={event => setEmail(event.target.value)} />
-			<input type="password" placeholder="enter password" onChange={event => setPassword(event.target.value)} />
+			{store.token ? <Redirect to="/signup" /> : ""}
+            <input type="text" placeholder="enter business name" onChange={event => setBusiness_name(event.target.value)} />
+            <input type="text" placeholder="enter business address" onChange={event => setAddress(event.target.value)} />
+			<input type="text" placeholder="enter business email" onChange={event => setEmail(event.target.value)} />            
+			<input type="text" placeholder="enter phone number" onChange={event => setPhone_number(event.target.value)} />            
+            <input type="password" placeholder="enter password" onChange={event => setPassword(event.target.value)} />
 			<input type="button" value="send" onClick={async () => actions.signup(email, password)} />
 		</div>
 	);
